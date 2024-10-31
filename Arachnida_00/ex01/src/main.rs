6a6c3259a91701f2b58321a7430d3d560ba19ed9
GitCGuillaume <guillaume2.chopin@orange.fr>
2024-10-31 18:47:54 +0100

    now crawling and registering images

--- Arachnida_00/ex01/src/main.rs
+++ Arachnida_00/ex01/src/main.rs
@@ -1,17 +1,17 @@
 use std::collections::HashMap ;
 use std::io::prelude::*;
 use std::fs::{ create_dir_all, File };
+use regex::Regex;
 use select::document::Document;
 use select::predicate::Name;
 use tokio;
 use reqwest::{ header::USER_AGENT, Client, Error, Response };
 use select::document;
 
-const ARR_TYPE: [&str; 5] = [
-    ".jpg", ".jpeg",
-    ".png", ".gif",
-    ".bmp"
-];
+struct OptionUser {
+    url: String,
+    folder: String
+}
 
 /* 
  * Trim spaces and removes useless values
@@ -79,7 +79,7 @@ fn  find_flags(_value: &String, mut _is_recursive: bool,
  * https://www.iana.org/assignments/media-types/media-types.xhtml#image
  * Check if content-type is valid
  */
-fn check_image_type(content_type: Option<&reqwest::header::HeaderValue>) -> bool {
+fn check_image_type(content_type: &Option<&reqwest::header::HeaderValue>) -> bool {
     let arr_type: [String; 5] = [
         "image/jpg".to_string(), "image/jpeg".to_string(),
         "image/png".to_string(), "image/gif".to_string(),
@@ -89,10 +89,11 @@ fn check_image_type(content_type: Option<&reqwest::header::HeaderValue>) -> bool
         Some(content) => {
             let name: Result<&str, reqwest::header::ToStrError> = content.to_str();
             let mut is_same = false;
+
             match name {
                 Ok(value) => {
+                    //println!("Type-content: {value}");
                     for i in arr_type {
-                        println!("{i} {value}");
                         if i == value {
                             is_same = true;
                         }
@@ -116,7 +117,10 @@ fn check_image_type(content_type: Option<&reqwest::header::HeaderValue>) -> bool
     Create directory and it's childs
 */
 fn create_dir(_dir_path: &mut String, id: usize) {
+    println!("d: {_dir_path}");
     let _file_name: String = _dir_path.split_off(id);
+    println!("dspl: {_dir_path}");
+    println!("{_file_name}");
     let result_dir: Result<(), std::io::Error> = create_dir_all(_dir_path);
 
     match result_dir {
@@ -138,7 +142,9 @@ fn  create_image(_img_path: &String, i: &[u8]) {
         Ok(mut f) => {
             let res: Result<(), std::io::Error> = f.write_all(i);
             match res {
-                Ok(result) => println!("{result:?}"),
+                Ok(_) => {
+                    println!("Image created")
+                },
                 Err(e) => {
                     eprintln!("Couldn't write to file: {e}");
                 },
@@ -151,16 +157,17 @@ fn  create_image(_img_path: &String, i: &[u8]) {
 }
 
 /* Start of image creation, call all functions to create an image */
-async fn process_image(i: Response) -> Result<(), Error> {
+async fn process_image(i: Response, options: &OptionUser) -> Result<(), Error> {
+    //println!("{i:?}");
     let content_type: Option<&reqwest::header::HeaderValue>
                             = i.headers().get("content-type");
-    let res: bool = check_image_type(content_type);
+    let res: bool = check_image_type(&content_type);
 
     if res == false {
-        eprintln!("File is not part of accepted type-content");
+        //eprintln!("File is not part of accepted type-content");
         return Ok(());
     }
-    let mut relative_path: String = ".".to_owned() + i.url().path();
+    let mut relative_path: String = options.folder.clone() + i.url().path();
     let path_clone = relative_path.clone();
     let idx: Option<usize> = relative_path.rfind("/");
 
@@ -183,15 +190,15 @@ async fn process_image(i: Response) -> Result<(), Error> {
 /*
  * Function wrapping image creation and validation
  */
-async fn send_url_file(_client: &Client, _path: &str) -> Result<(), Error> {
+async fn send_url_file(_client: &Client, options: &OptionUser, _path: &str,) -> Result<(), Error> {
     let res: Result<Response, Error> = _client.get(_path)
         .header(USER_AGENT, "Reqwest/0.12.8")
         .send().await;
-    ;
+
     let res: Result<(), Error> = match res {
         Ok(i) => {
-            println!("azeezz {:?}", i);
-            let _ = process_image(i).await;
+            let _ = process_image(i, options).await;
+
             Ok(())
         },
         Err(e) => {
@@ -205,44 +212,30 @@ async fn send_url_file(_client: &Client, _path: &str) -> Result<(), Error> {
 /*
  * Call server (request) to parse
  */
-async fn get_url_text(_client: &Client, _path: &str) -> Result<String, Error> {
-    let res: Result<String, Error> = match _client.get(_path).header(USER_AGENT, "Reqwest/0.12.8").send().await {
-        Ok(value) => {
-            value.text().await
-        },
-        Err(e) => {
-            Err(e)
-        },
-    };
+async fn get_url_header(_client: &Client, _path: &str) -> Result<Response, Error> {
+    let res: Result<Response, Error> = _client.get(_path).header(USER_AGENT, "Reqwest/0.12.8").send().await;
+
     res
 }
 
-/*
- * Resolve array and return a boolean
- */
-fn  url_extension_resolver(_path: &str) -> bool {
-    for i in ARR_TYPE {
-        if _path.ends_with(i) {
-            println!("FOUND");
-            return true;
-        }
-    }
-    false
-}
-
-fn  try_insert_hmap(hmap_url: &mut HashMap<String, bool>, f: &String) {
+fn  try_insert_hmap(hmap_url: &mut HashMap<String, bool>, f: &String, is_img: bool) {
     let k: Option<(&String, &bool)> = hmap_url.get_key_value(f);
-println!("dd: {f}");
+
     match k {
         Some(_) => (),
         None => {
-            hmap_url.insert(f.to_owned(), false);
+            if is_img {
+                hmap_url.insert(f.to_owned(), true);
+            } else {
+                hmap_url.insert(f.to_owned(), false);
+            }
         },
     }
 }
-//^[https://].*/
+
 fn  get_links(_url: &String, _doc: &Document,
              hmap_url: &mut HashMap<String, bool>) -> bool {
+    let regex = Regex::new(r"^https?://[\w\d.-]*");
     let a_dom: document::Find<'_, Name<&str>> = _doc.find(Name("a"));
     let mut find_input: bool = false;
 
@@ -250,94 +243,94 @@ fn  get_links(_url: &String, _doc: &Document,
         .for_each(|f| {
             find_input = true;
             if !f.starts_with("http") {
-                try_insert_hmap(hmap_url, &String::from(_url.clone() + f));
+                match &regex {
+                    Ok(reg) => {
+                        let aa = reg.captures(_url).unwrap().get(0).unwrap().as_str();
+                        let bb = String::from(aa) + f;
+
+                        try_insert_hmap(hmap_url, &bb, false);
+                    },
+                    Err(_) => {},
+                };
             } else {
-                try_insert_hmap(hmap_url, &f.to_owned());
+                try_insert_hmap(hmap_url, &f.to_owned(), false);
+
             }
         });
     find_input
 }
 
-async fn  get_images(_url: &String, cli: &Client,
-               _doc: &Document, hmap_url: &mut HashMap<String, bool>) {
+async fn  get_images(options: &OptionUser, cli: &Client,
+                     _doc: &Document, hmap_url: &mut HashMap<String, bool>) {
+    let regex = Regex::new(r"^https?://[\w\d.-]*");
     let img_dom: document::Find<'_, Name<&str>> = _doc.find(Name("img"));
-    //let mut find_input: bool = false;
 
-        for f in img_dom.filter_map(|f| f.attr("src")) {
-          //  find_input = true;
-            println!("ele: {f}");
-            if !f.starts_with("http") {
-                //get parent url
-                println!("{f}");
-                /*try_insert_hmap(hmap_url, &String::from(_url.clone() + f));
-                if url_extension_resolver(&String::from(_url.clone() + f)) {
-                    let _ = send_url_file(cli, &String::from(_url.clone() + f));
-                }*/
-            } else {
-                try_insert_hmap(hmap_url, &f.to_owned());
-                if url_extension_resolver(&f) {
-                    println!("NII");
-                    let _ = send_url_file(cli, &f).await;
-                }
-            }
+    for f in img_dom.filter_map(|f| f.attr("src")) {
+        if !f.starts_with("http") {
+            match &regex {
+                Ok(reg) => {
+                    let aa = reg.captures(&options.url).unwrap().get(0).unwrap().as_str();
+                    let bb = String::from(aa) + f;
+
+                    try_insert_hmap(hmap_url, &bb, true);
+                    let _ = send_url_file(cli, options, &bb).await;
+                },
+                Err(_) => {},
+            };
+        } else {
+                try_insert_hmap(hmap_url, &f.to_owned(), true);
+                let _ = send_url_file(cli, options, &f).await;
         }
-    
-        /* .for_each(| f| {
-            find_input = true;
-            println!("{f}");
-            if !f.starts_with("http") {
-                //get parent url
-                println!("{f}");
-                /*try_insert_hmap(hmap_url, &String::from(_url.clone() + f));
-                if url_extension_resolver(&String::from(_url.clone() + f)) {
-                    let _ = send_url_file(cli, &String::from(_url.clone() + f));
-                }*/
-            } else {
-                try_insert_hmap(hmap_url, &f.to_owned());
-                if url_extension_resolver(&f) {
-                    println!("NII");
-                    let _ = send_url_file(cli, &f).await;
-                }
-            }
-        });*/
+    }
 }
 
-async fn parse_document(_url: &String, cli: &Client,
+async fn parse_document(options: &OptionUser, cli: &Client,
                  text: &String, hmap_url: &mut HashMap<String, bool>) -> bool {
     let doc: Document = document::Document::from(text.as_str());
     let mut _find_input: bool = false;
 
-    //_find_input = get_links(_url, &doc, hmap_url);
-    get_images(_url, cli, &doc, hmap_url).await;
+    _find_input = get_links(&options.url, &doc, hmap_url);
+    get_images(options, cli, &doc, hmap_url).await;
     _find_input
 }
 
-async fn url_helper(_url: &String, cli: &Client,
+async fn url_helper(options: &OptionUser, cli: &Client,
     hmap_url: &mut HashMap<String, bool>) -> bool {
     let mut find_input: bool = false;
 
     for (k, _v) in hmap_url.clone() {
-        println!("Url: {k}");
         if _v == true {
             continue;
         }
+        println!("Url open: {k}");
         hmap_url.insert(k.to_owned(), true);
-        if url_extension_resolver(&k) {
-            let _ = send_url_file(cli, &k).await;
-        } else {
-            let res: Result<String, Error> = get_url_text(cli, &k).await;
-    
-            match res {
-                Ok(r) => {
-                    find_input = parse_document(_url, cli, &r, hmap_url).await;
-                },
-                Err(e) => {
-                    eprintln!("Error: {e}");
-                },
-            }
+        let res: Result<Response, Error> = get_url_header(cli, &k).await;
+
+        match res {
+            Ok(r) => {
+                let content_type: Option<&reqwest::header::HeaderValue>
+                            = r.headers().get("content-type");
+                let res: bool = check_image_type(&content_type);
+                if !res {
+                    let txt: Result<String, Error> = r.text().await;
+
+                    match txt {
+                        Ok(r) => {
+                            find_input = parse_document(&options, cli, &r, hmap_url).await;
+                        },
+                        Err(e) => {
+                            eprintln!("Get Url Error: {e}");
+                        },
+                    }
+                } else {
+                    let _ = process_image(r, options).await;
+                }
+            },
+            Err(e) => {
+                eprintln!("Error: {e}");
+            },
         }
     }
-    println!("");
     find_input
 }
 
@@ -347,9 +340,13 @@ async fn connect_client(_path: &String) -> Result<Client, Error> {
     return client
 }
 
-async fn connect(_url: &String, _max_depth: i32) {
+async fn connect(_url: &String, path: &String, _max_depth: i32) {
     let mut hmap_url: HashMap<String, bool> = HashMap::new();
     let client: &Result<Client, Error> = &connect_client(&_url).await;
+    let options: OptionUser = OptionUser {
+        url: _url.clone(),
+        folder: path.clone()
+    };
 
     hmap_url.insert(_url.clone(), false);
     match client {
@@ -357,7 +354,8 @@ async fn connect(_url: &String, _max_depth: i32) {
             let mut _find_input: bool = false;
 
             for _i in 0.._max_depth {
-                _find_input = url_helper(_url, &c, &mut hmap_url).await;
+                println!("i: {_i}");
+                _find_input = url_helper(&options, &c, &mut hmap_url).await;
                 if !_find_input {
                     break ;
                 }
@@ -401,7 +399,7 @@ async fn main() {
         let max_depth: Result<i32, _> = max_depth.parse();
         match max_depth {
             Ok(max) => {
-                connect(&url, max).await;
+                connect(&url, &path, max).await;
             },
             Err(_) => {
                 eprint!("Please provide a valid max depth.");
