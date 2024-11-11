b1c2793ce013688d4ec8bc5ef604946828624012
guillaume <guillaume@desktop-13efv45.home>
2024-11-11 17:05:57 +0100

    Start ft_otp

---
+++ ft_otp/src/main.rs
@@ -0,0 +1,8 @@
+//https://datatracker.ietf.org/doc/html/rfc6238#page-2
+//Must be TOTP but must be based on HOTP
+//Check initial hexadecimal key K
+//TOTP(K,T)
+//Generated code must be 6 digits using HMAC-SHA-1
+fn main() {
+    println!("Hello, world!");
+}
