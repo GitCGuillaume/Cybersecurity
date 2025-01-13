01fa5433281490fcdfffa0932f549673c429ddf3
guillaume <guillaume@desktop-13efv45.home>
2025-01-13 15:42:37 +0100

    Level2 cracked

--- reverse_me/level2/source.c
+++ reverse_me/level2/source.c
@@ -1,21 +1,3 @@
-//atoi
-//printf
-//scanf
-//strcmp
-//memset
-//main
-//fflush
-//exit
-//strlen
-//ok
-//n
-//no
-//xd
-//xxd
-//xxxd
-//ww
-//xyxxd
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -37,14 +19,18 @@ void    ok() {
 //101
 //114
 //101
+//https://faydoc.tripod.com/cpu/jae.htm
 int main(void) {
     int a;
     char c[100];
     char d[9];
+    size_t j;
+    int i;
+
     a = 0;
     printf("Please enter key:\n");
     a = scanf("%23s", c);
-    if (a == 0)
+    if (a != 1)
         no();
     if (c[1] != '0') {
         no();
@@ -54,44 +40,36 @@ int main(void) {
     }
     fflush(stdin);
     memset(d, 0, sizeof(d));
-    d[0] = 0x64:
+    d[0] = 'd';
     d[1] = 0;
-    d[2] = 0x2;
-    d[3] = 0x1;
-
-    //strlen(char)
-    //strlen(str entière)
-    //cmp 0x8
-    //atoi(ABC)atoi(DEF...)
-    //addition pour créer delabere
-    //strcmp()
+    j = 0x2;
+    i = 0x1;
     while (1) {
-        a = 0;
-        if (strlen(d) != 0x8) {
-            a = strlen(c);//ecx:23 eax:2 puis 5
-            //test 0x1 $al (test $al & 1)
-            //jne
-            //jmp(break?)
+        a = strlen(d);
+        int b = 0;
+        if (!(a >= 0x8)) {
+            //cmp ecx(23) eax(j)
+            //setb
+            b = j < strlen(c);//set à 1
         }
-        //cmp (test & 1)
-        if (!(a & 1))
+        //test 0x1 $al (test $al & 1)
+        //jne
+        //jmp(break?)
+        //line 286 test & 1
+        if (!(b & 1))
             break ;
-        int a1 = c[a];
-        int a2 = c[a + 1];
-        int a3 = c[a + 2];
-        //atoi
-        //c++?
-        i++;
+        char e[4];
+        e[0] = c[j];
+        e[1] = c[j + 1];
+        e[2] = c[j + 2];
+        e[3] = 0;
+        a = atoi(e);
+        j += 3;
+        d[i] = a;
+        i += 1;
     }
-    if (strcmp(d), "delabere")
+    if (strcmp(d, "delabere"))
         no();
     ok();
-    //a = strcmp(&c[2], "delabere");
-    printf("%x\n", '\n');
-    printf("res:%d\n", atoi("del"));
-
-    printf("res:%d\n", atoi("abe"));
-
-    printf("res:%d\n", atoi("re"));
     return 0;
 }
